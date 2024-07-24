6e4d4259f2e68e6c5cb4bbbce3e8f12d7fcdf0a5
Documenter.jl <documenter@juliadocs.github.io>
2024-07-24 20:20:01 +0000

    build based on 07db83b

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = SparseQuadrature","category":"page"},{"location":"#SparseQuadrature","page":"Home","title":"SparseQuadrature","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for SparseQuadrature.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [SparseQuadrature]","category":"page"}]
+}
